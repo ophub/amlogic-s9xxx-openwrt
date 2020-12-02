b513020b4a873d3e210d477188e5ee94a4518f10
ophub <68696949+ophub@users.noreply.github.com>
2020-12-02 18:00:27 +0800

    Update diy-part2.sh

--- router/diy-part2.sh
+++ router/diy-part2.sh
@@ -21,17 +21,3 @@
 # ==========luci-theme-url==========
 # svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod package/luci-theme-bootstrap-mod
 
-
-#===== Recommended packages, Start =====
-packages=" \
-brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs antfs-mount \
-kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
-blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip lscpu htop iperf3 curl lm-sensors 
-"
-
-sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' target/linux/armvirt/Makefile
-for x in $packages; do
-    sed -i "/DEFAULT_PACKAGES/ s/$/ $x/" target/linux/armvirt/Makefile
-done
-#===== Recommended packages, End =====
-

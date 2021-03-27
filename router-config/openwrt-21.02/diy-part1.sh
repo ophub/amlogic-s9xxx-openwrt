c4072cb2d476d2e0a31225f4912ebe180f10ef99
ophub <chinaue@gmail.com>
2021-03-27 19:47:09 +0800

    update

--- router-config/openwrt-21.02/diy-part1.sh
+++ router-config/openwrt-21.02/diy-part1.sh
@@ -8,7 +8,7 @@
 #========================================================================================================================
 
 # Uncomment a feed source
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
 # Add a feed source

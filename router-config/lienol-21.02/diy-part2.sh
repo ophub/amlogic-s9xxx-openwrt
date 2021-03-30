521dd2b81a67177f777b49c763833f951470a106
ophub <chinaue@gmail.com>
2021-03-30 20:38:16 +0800

    update

--- router-config/lienol-21.02/diy-part2.sh
+++ router-config/lienol-21.02/diy-part2.sh
@@ -16,6 +16,10 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 # sed -i 's/root::0:0:99999:7:::/root:$1$IdsfksW7$LoTjdTJSZz\/IhFyw.t9wj1:18713:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# Add branches package
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
+
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
 

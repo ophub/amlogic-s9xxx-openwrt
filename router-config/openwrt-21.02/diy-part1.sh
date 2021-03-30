85ea02396af89747f49e69a752501a90a9107d86
ophub <chinaue@gmail.com>
2021-03-30 20:38:16 +0800

    update

--- router-config/openwrt-21.02/diy-part1.sh
+++ router-config/openwrt-21.02/diy-part1.sh
@@ -7,12 +7,9 @@
 # Copyright (C) 2020 https://github.com/ophub/amlogic-s9xxx-openwrt
 #========================================================================================================================
 
-# Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
-
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i 's/src-git packages/#src-git packages/g' ./feeds.conf.default
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}

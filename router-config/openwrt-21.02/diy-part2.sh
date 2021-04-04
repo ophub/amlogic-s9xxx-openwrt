2ebd7cf4446f1dcc2dbce2509899752b35b3b612
ophub <68696949+ophub@users.noreply.github.com>
2021-04-04 23:59:59 +0800

    Update diy-part2.sh

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -61,12 +61,14 @@ svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passw
 # Convert translation files zh-cn to zh_Hans
 # The script is still in testing, welcome to report bugs.
 
+convert_files=0
 po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po"; } 2>"/dev/null")"
 for a in ${po_file}
 do
     [ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
     po_new_file="$(echo -e "$a"|sed "s/zh-cn/zh_Hans/g")"
     mv "$a" "${po_new_file}" 2>"/dev/null"
+    let convert_files++
 done
 
 po_file2="$({ find |grep "/zh-cn/" |grep "\.po"; } 2>"/dev/null")"
@@ -75,6 +77,7 @@ do
     [ -n "$(grep "Language: zh_CN" "$b")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$b"
     po_new_file2="$(echo -e "$b"|sed "s/zh-cn/zh_Hans/g")"
     mv "$b" "${po_new_file2}" 2>"/dev/null"
+    let convert_files++
 done
 
 lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo"; } 2>"/dev/null")"
@@ -82,6 +85,7 @@ for c in ${lmo_file}
 do
     lmo_new_file="$(echo -e "$c"|sed "s/zh_Hans/zh-cn/g")"
     mv "$c" "${lmo_new_file}" 2>"/dev/null"
+    let convert_files++
 done
 
 lmo_file2="$({ find |grep "/zh_Hans/" |grep "\.lmo"; } 2>"/dev/null")"
@@ -89,6 +93,7 @@ for d in ${lmo_file2}
 do
     lmo_new_file2="$(echo -e "$d"|sed "s/zh_Hans/zh-cn/g")"
     mv "$d" "${lmo_new_file2}" 2>"/dev/null"
+    let convert_files++
 done
 
 po_dir="$({ find |grep "/zh-cn" |sed "/\.po/d" |sed "/\.lmo/d"; } 2>"/dev/null")"
@@ -96,6 +101,7 @@ for e in ${po_dir}
 do
     po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
     mv "$e" "${po_new_dir}" 2>"/dev/null"
+    let convert_files++
 done
 
 makefile_file="$({ find|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
@@ -103,6 +109,8 @@ for f in ${makefile_file}
 do
     [ -n "$(grep "zh-cn" "$f")" ] && sed -i "s/zh-cn/zh_Hans/g" "$f"
     [ -n "$(grep "zh_Hans.lmo" "$f")" ] && sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$f"
+    let convert_files++
 done
 
-echo -e "Convert translation files zh-cn to zh_Hans to complete."
+echo -e "Convert translation files zh-cn to zh_Hans to complete. ${convert_files} in total."
+

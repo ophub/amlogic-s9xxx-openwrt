606b362fd43706801c55c4f6335d5d4a6e30f61e
ophub <chinaue@gmail.com>
2021-04-18 14:27:42 +0800

    update

---
+++ router-config/openwrt-master/diy-part1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic S9xxx STB
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
+# Copyright (C) 2020 https://github.com/ophub/amlogic-s9xxx-openwrt
+#========================================================================================================================
+
+# Add a feed source
+# sed -i 's/src-git packages/#src-git packages/g' ./feeds.conf.default
+# sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
+
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+

81f3cc9df667812dbd3e960f8de38dfd375c1eaa
ophub <68696949+ophub@users.noreply.github.com>
2021-05-12 15:38:38 +0800

    Update diy-part2.sh

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -43,7 +43,7 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/o
 pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
 
 # # Add luci-app-ssr-plus
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-ssrplus
+svn co https://github.com/fw876/helloworld/trunk/{luci-app-ssr-plus,shadowsocksr-libev} package/openwrt-ssrplus
 
 # Add luci-app-rclone
 svn co https://github.com/ElonH/Rclone-OpenWrt/trunk package/openWrt-rclone

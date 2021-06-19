74e46f8dcc8c907667406969631a8e7299a02ced
ophub <chinaue@gmail.com>
2021-06-19 08:10:41 +0800

    Kernel: bump to 5.12.12 / 5.4.127 / 5.10.45

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -3,6 +3,7 @@
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt for Amlogic S9xxx STB
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
 # Uncomment a feed source

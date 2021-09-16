8e86946bb331b2ca1838977298c288e1a8b44c28
zaiyin93 <90806452+zaiyin93@users.noreply.github.com>
2021-09-16 12:54:00 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -111,16 +111,16 @@ wget -O package/base-files/files/bin/fixphp "https://raw.githubusercontent.com/h
 
 
 # Mod zzz-default-settings for HelmiWrt
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-sed -i "s/${orig_version}/${orig_version} ${date_version}/g" zzz-default-settings
-sed -i "s/zh_cn/auto/g" zzz-default-settings
-sed -i "s/uci set system.@system[0].timezone=CST-8/uci set system.@system[0].hostname=HelmiWrt\nuci set system.@system[0].timezone=WIB-7/g" zzz-default-settings
-sed -i "s/Shanghai/Jakarta/g" zzz-default-settings
-popd
+# pushd package/lean/default-settings/files
+# sed -i '/http/d' zzz-default-settings
+# sed -i '/18.06/d' zzz-default-settings
+# export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
+# sed -i "s/${orig_version}/${orig_version} ${date_version}/g" zzz-default-settings
+# sed -i "s/zh_cn/auto/g" zzz-default-settings
+# sed -i "s/uci set system.@system[0].timezone=CST-8/uci set system.@system[0].hostname=HelmiWrt\nuci set system.@system[0].timezone=WIB-7/g" zzz-default-settings
+# sed -i "s/Shanghai/Jakarta/g" zzz-default-settings
+# popd
 
 
 

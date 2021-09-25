b515fbb86afb4fb90620c1211b9ef23675602879
ophub <ophub2020@gmail.com>
2021-09-25 16:44:17 +0800

    tunning balethirq.pl and Add setting file system type

--- amlogic-s9xxx/common-files/patches/balethirq/balethirq.pl
+++ amlogic-s9xxx/common-files/patches/balethirq/balethirq.pl
@@ -19,28 +19,28 @@ sub read_config {
     my $fh;
     my $config_file = "/etc/config/balance_irq";
     if( -f $config_file) {
-    	open $fh, "<", $config_file or die $!;
-    	while(<$fh>) {
-		chomp;
-		my($name, $value) = split;
-		my @cpus = split(',', $value);
-		my $min_cpu = 9;
+            open $fh, "<", $config_file or die $!;
+            while(<$fh>) {
+                chomp;
+                my($name, $value) = split;
+                my @cpus = split(',', $value);
+                my $min_cpu = 9;
 
-		foreach my $cpu (@cpus) {
-		    if($cpu > $cpu_count) {
-			$cpu = $cpu_count;	
-		    } elsif($cpu < 1) {
-			$cpu = 1;
-		    }
-		    if($min_cpu > $cpu) {
+                foreach my $cpu (@cpus) {
+                    if($cpu > $cpu_count) {
+                        $cpu = $cpu_count;        
+                    } elsif($cpu < 1) {
+                        $cpu = 1;
+                    }
+                    if($min_cpu > $cpu) {
                         $min_cpu = $cpu;
-		    }
-		}
+                    }
+                }
 
-		$cpu_map{$name} = \@cpus;
-		$min_cpu_map{$name} = $min_cpu;
-	}	
-    	close $fh;
+                $cpu_map{$name} = \@cpus;
+                $min_cpu_map{$name} = $min_cpu;
+        }        
+            close $fh;
     } 
 }
 
@@ -49,12 +49,12 @@ sub get_cpu_count {
     open $fh, "<", "/proc/cpuinfo" or die $!;
     my $count=0;
     while(<$fh>) {
-	    chomp;
-	    my @ary = split;
-	    if($ary[0] eq "processor") {
-		    $count++;
-		    $all_cpu_mask += 1<<($count-1);
-	    }
+        chomp;
+        my @ary = split;
+        if($ary[0] eq "processor") {
+            $count++;
+            $all_cpu_mask += 1<<($count-1);
+        }
     }
     close $fh;
     return $count;
@@ -64,98 +64,95 @@ sub read_irq_data {
     my $fh;
     open $fh, "<", "/proc/interrupts" or die $!;
     while(<$fh>) {
-	    chomp;
-	    my @raw = split;
-	    my $irq = $raw[0];
-  	    $irq =~ s/://;
-	    my $name = $raw[-1];
+        chomp;
+        my @raw = split;
+        my $irq = $raw[0];
+        $irq =~ s/://;
+        my $name = $raw[-1];
 
-	    if(exists $cpu_map{$name}) {
-		$irq_map{$name} = $irq;
-		if($name =~ m/\Aeth[0-9]\Z/) {
-		    $uniq_eth_cpu_map{$name} = 1 << ($min_cpu_map{$name} - 1);
-		} elsif($name =~ m/\Axhci-hcd:usb[1-9]\Z/) { # usb extend eth1
-		    $uniq_eth_cpu_map{eth1} =  1 << ($min_cpu_map{$name} - 1);
-		}
-	    }
+        if(exists $cpu_map{$name}) {
+            $irq_map{$name} = $irq;
+            if($name =~ m/\Aeth[0-9]\Z/) {
+                $uniq_eth_cpu_map{$name} = 1 << ($min_cpu_map{$name} - 1);
+            } elsif($name =~ m/\Axhci-hcd:usb[1-9]\Z/) { # usb extend eth1
+                $uniq_eth_cpu_map{eth1} =  1 << ($min_cpu_map{$name} - 1);
+            }
+        }
     }
     close $fh;
 }
 
 sub update_smp_affinity {
     for my $key (sort keys %irq_map) {
-    	my $fh;
-	my $irq = $irq_map{$key};
-	my $cpus_ref = $cpu_map{$key};
-	my $mask = 0;
-	foreach my $cpu (@$cpus_ref) {
-	    $mask += 1 << ($cpu-1);
-	}
-	my $smp_affinity = sprintf("%0x", $mask);
-    	open $fh, ">", "/proc/irq/$irq/smp_affinity" or die $!;
-	print "irq name:$key, irq:$irq, affinity: $smp_affinity\n";
-	print $fh "$smp_affinity\n";
-    	close $fh;
+        my $fh;
+        my $irq = $irq_map{$key};
+        my $cpus_ref = $cpu_map{$key};
+        my $mask = 0;
+        foreach my $cpu (@$cpus_ref) {
+            $mask += 1 << ($cpu-1);
+        }
+        my $smp_affinity = sprintf("%0x", $mask);
+        open $fh, ">", "/proc/irq/$irq/smp_affinity" or die $!;
+        print "irq name:$key, irq:$irq, affinity: $smp_affinity\n";
+        print $fh "$smp_affinity\n";
+        close $fh;
     }
 }
 
 sub tunning_eth_ring {
-    my $eth = shift;
+    my ($eth, $target_rx_ring, $target_tx_ring) = @_;
     my $buf = `/usr/sbin/ethtool -g ${eth} 2>/dev/null`;
-    if($? == 0) {
+    if($buf) {
         $buf =~ s/\r|\n/\t/g;
-	if( $buf =~ m/.+?Pre-set maximums:\s+RX:\s+(\d+).+?TX:\s+(\d+).+?Current hardware settings:\s+RX:\s+(\d+).+?TX:\s+(\d+)/) {
+        if( $buf =~ m/.+?Pre-set maximums:\s+RX:\s+(\d+).+?TX:\s+(\d+).+?Current hardware settings:\s+RX:\s+(\d+).+?TX:\s+(\d+)/) {
             my $max_rx_ring  = $1;
             my $max_tx_ring  = $2;
             my $cur_rx_ring  = $3;
             my $cur_tx_ring  = $4;
 
-   	    my $target_rx_ring = $max_rx_ring / 2;
-	    my $target_tx_ring = $max_tx_ring / 2;
-
-	    if( ($max_rx_ring > 0) && ($cur_rx_ring != $target_rx_ring ) ) {
-	        system "/usr/sbin/ethtool -G ${eth} rx ${target_rx_ring} >/dev/null 2>&1";
+            if( ($max_rx_ring > 0) && ($target_rx_ring>0) && ($max_rx_ring > $target_rx_ring) && ($cur_rx_ring != $target_rx_ring) ) {
+                system "/usr/sbin/ethtool -G ${eth} rx ${target_rx_ring} >/dev/null 2>&1";
+                print "Set the rx ring of ${eth} to ${target_rx_ring}\n";
             }
-	    if( ($max_tx_ring > 0) && ($cur_tx_ring != $target_tx_ring ) ) {
-	        system "/usr/sbin/ethtool -G ${eth} tx ${target_tx_ring} >/dev/null 2>&1";
+            if( ($max_tx_ring > 0) && ($target_tx_ring>0) && ($max_tx_ring > $target_tx_ring) && ($cur_tx_ring != $target_tx_ring) ) {
+                system "/usr/sbin/ethtool -G ${eth} tx ${target_tx_ring} >/dev/null 2>&1";
+                print "Set the tx ring of ${eth} to ${target_tx_ring}\n";
             }
-	}
+        }
     } 
 }
 
 sub enable_eth_rps_rfs {
     my $rps_sock_flow_entries = 0;
     for my $eth ("eth0","eth1") {
-
         if(-d "/sys/class/net/${eth}/queues/rx-0") {
-	    my $value = 4096;
+            my $value = 32768;
             $rps_sock_flow_entries += $value;
             my $eth_cpu_mask_hex;
- 	    #if($eth eq "eth0") {
- 	    #    $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask - $uniq_eth_cpu_map{$eth} - $uniq_eth_cpu_map{eth1});
- 	    #} else {
- 	    #    $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask - $uniq_eth_cpu_map{$eth});
- 	    #}
- 	    
-	    $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask);
-	    print "Set the rps cpu mask for $eth to 0x$eth_cpu_mask_hex\n";
-	    open my $fh, ">", "/sys/class/net/${eth}/queues/rx-0/rps_cpus" or die;
-	    print $fh $eth_cpu_mask_hex;
-	    close $fh;
+            #if($eth eq "eth0") {
+            #    $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask - $uniq_eth_cpu_map{$eth} - $uniq_eth_cpu_map{eth1});
+            #} else {
+	    #    $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask - $uniq_eth_cpu_map{$eth});
+            #}
+	    
+            $eth_cpu_mask_hex = sprintf("%0x", $all_cpu_mask - $uniq_eth_cpu_map{$eth});
+            print "Set the rps cpu mask of $eth to 0x$eth_cpu_mask_hex\n";
+            open my $fh, ">", "/sys/class/net/${eth}/queues/rx-0/rps_cpus" or die;
+            print $fh $eth_cpu_mask_hex;
+            close $fh;
 
-	    open $fh, ">", "/sys/class/net/${eth}/queues/rx-0/rps_flow_cnt" or die;
-	    print $fh $value;
-	    close $fh;
+            open $fh, ">", "/sys/class/net/${eth}/queues/rx-0/rps_flow_cnt" or die;
+            print $fh $value;
+            close $fh;
 
-	    open my $fh, ">", "/sys/class/net/${eth}/queues/tx-0/xps_cpus" or die;
-	    print $fh $eth_cpu_mask_hex;
-	    close $fh;
+            open my $fh, ">", "/sys/class/net/${eth}/queues/tx-0/xps_cpus" or die;
+            print $fh $eth_cpu_mask_hex;
+            close $fh;
 
-        #&tunning_eth_ring($eth) if ($eth ne "eth0");
+            &tunning_eth_ring($eth, 256, 0) if ($eth ne "eth0");
         }
     }
     open my $fh, ">", "/proc/sys/net/core/rps_sock_flow_entries" or die;
     print $fh $rps_sock_flow_entries;
     close $fh;
 }
-

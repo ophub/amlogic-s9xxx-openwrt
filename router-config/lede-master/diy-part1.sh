63067ccbc1dac0d67f99caf40898d11906e28ae4
onwingslnz <leenaizhe@gmail.com>
2021-10-07 16:49:20 +0800

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -12,14 +12,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-<<<<<<< HEAD
 # sed -i '$a src-git liuran001_packages https://github.com/onwingslnz/openwrt-packages' feeds.conf.default
-
-# Add dockerman
-sed -i '$a src-git lienol https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker' feeds.conf.default
-=======
->>>>>>> parent of 94ec725 (20211007 update)
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}

488f3739793708b4862f125c00e48deb87b001d1
Manssizz <fajarmanssizz@gmail.com>
2021-10-11 13:13:52 +0700

    Replace ffmpeg with openwrt source

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -51,6 +51,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # Add p7zip
 # svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 
+# Replace ffmpeg
+rm -rf feeds/packages/multimedia/ffmpeg
+svn checkout https://github.com/openwrt/packages/trunk/multimedia/ffmpeg feeds/packages/multimedia/ffmpeg
+
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
 # svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils

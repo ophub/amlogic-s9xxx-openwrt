49d05e734f6b332fb1719231ce041a0ca631a903
ophub <ophub2020@gmail.com>
2021-10-28 19:03:07 +0800

    Add OpenWrt official 21.02 branch

---
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic S9xxx STB
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: 21.02
+#========================================================================================================================
+
+# ------------------------------- Main source started -------------------------------
+#
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Set DISTRIB_REVISION
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
+# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+
+# Replace the default software source
+# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk package/luci-app-amlogic
+
+# Add luci-app-cpufreq
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cpufreq package/lean/luci-app-cpufreq
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+
+# Add autocore
+svn co https://github.com/breakings/OpenWrt/trunk/general/autocore package/lean/autocore
+
+# Add p7zip
+# svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
+
+# coolsnowwolf default software package replaced with Lienol related software package
+# rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
+# svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
+
+# Add third-party software packages (The entire repository)
+# git clone https://github.com/libremesh/lime-packages.git package/lime-packages
+# Add third-party software packages (Specify the package)
+# svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
+# Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
+# sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
+
+# Apply patch
+# git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
+#
+# ------------------------------- Other ends -------------------------------
+

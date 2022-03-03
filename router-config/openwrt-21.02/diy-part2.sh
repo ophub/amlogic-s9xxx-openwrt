adb3f1393bd79305ccafdbb550116b33214a4604
ophub <ophub2020@gmail.com>
2022-03-03 12:42:38 +0800

    Adjust the default dtb of Beelink-Mini-MX

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -19,7 +19,7 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.30.6/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
 
 #
 # ------------------------------- Main source ends -------------------------------

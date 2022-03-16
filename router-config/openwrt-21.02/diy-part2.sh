9adc1fe820f20221a4d0fba43abaa778cf845847
ludfi25 <64370156+ludfi25@users.noreply.github.com>
2022-03-17 06:43:12 +0700

    Update diy-part2.sh

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -11,8 +11,8 @@
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
-# Add luci-app-passwall
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/Passwall-Openclash/main/install.sh" -O ~/install.sh && chmod 777 ~/install.sh && ~/./install.sh
+
+
 
 # Add luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash

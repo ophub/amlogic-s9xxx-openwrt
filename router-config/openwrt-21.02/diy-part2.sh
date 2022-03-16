d7784c570f40aa648d70a39ec4b2bf90e7eb8a81
ludfi25 <64370156+ludfi25@users.noreply.github.com>
2022-03-17 01:44:30 +0700

    Update diy-part2.sh

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
 # Add luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/Passwall-Openclash/main/install.sh" -O ~/install.sh && chmod 777 ~/install.sh && ~/./install.sh
 
 # Add luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash

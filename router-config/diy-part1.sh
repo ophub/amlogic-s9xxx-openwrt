905b147095505fddb7ccde9546ab029e4a67c641
ludfi25 <64370156+ludfi25@users.noreply.github.com>
2022-03-20 15:28:00 +0700

    Add files via upload

---
+++ router-config/diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: 21.02
+#========================================================================================================================
+
+# Uncomment a feed source
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+

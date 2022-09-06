f41eb984fb99c315db32bfa6785e0dd5af594eec
Believerd <30063871+believerd@users.noreply.github.com>
2022-09-06 15:26:23 +0800

    Update diy-part1.sh
    added dependencies

--- router-config/openwrt-master/diy-part1.sh
+++ router-config/openwrt-master/diy-part1.sh
@@ -13,7 +13,8 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # other

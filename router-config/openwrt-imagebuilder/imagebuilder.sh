e8760117a1e4644613249d952aea046deb36eaa5
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2022-09-29 15:04:42 +0700

    Update imagebuilder.sh

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -158,27 +158,27 @@ rebuild_firmware() {
     # Selecting default packages, lib, theme, app and i18n, etc.
     # sorting by https://build.moz.one
     my_packages="\
-        acpid attr base-files bash bc bind-server blkid block-mount blockd bsdtar  \
-        btrfs-progs busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark  \
-        coreutils coreutils-base64 coreutils-nohup coreutils-truncate curl docker  \
-        docker-compose dockerd dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs  \
-        f2fs-tools f2fsck fdisk gawk getopt gzip hostapd-common iconv iw iwinfo jq jshn  \
-        kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script  \
-        liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs  \
-        mount-utils openssl-util parted perl-http-date perlbase-file perlbase-getopt  \
-        perlbase-time perlbase-unicode perlbase-utf8 pigz ppp ppp-mod-pppoe  \
-        proto-bonding pv rename resize2fs runc subversion-client subversion-libs tar  \
-        tini ttyd tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch  \
-        uuidgen wget-ssl whereis which wpa-cli wpad-basic wwan xfs-fsck xfs-mkfs xz  \
-        xz-utils ziptool zoneinfo-asia zoneinfo-core zstd  \
+        base-files bash blkid block-mount  \
+        btrfs-progs busybox bzip2 cgi-io comgt comgt-ncm  \
+        coreutils coreutils-base64 coreutils-nohup curl  \
+        dosfstools e2fsprogs fdisk  \
+        gzip hostapd-common iconv iw iwinfo jq jshn  \
+        kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp liblucihttp-lua  \
+        losetup lsblk mkf2fs openssl-util parted  \
+        perlbase-file perlbase-time  \
+        perlbase-utf8 ppp ppp-mod-pppoe pv  \
+        tar ttyd uclient-fetch uhttpd  \
+        uhttpd-mod-ubus unzip uqmi usb-modeswitch uuidgen wget-ssl wpa-cli  \
+        wwan xz xz-utils zoneinfo-asia  \
+        zoneinfo-core zstd  \
         \
-        luci luci-base luci-compat luci-i18n-base-en luci-i18n-base-zh-cn luci-lib-base  \
-        luci-lib-docker luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
+        luci luci-base luci-compat luci-i18n-base-en luci-lib-base  \
+        luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
-        luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
-        luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
+        luci-proto-3g luci-proto-ipv6 luci-proto-ncm  \
+        luci-proto-ppp luci-proto-qmi luci-proto-modemmanager  \
         \
-        luci-app-amlogic luci-i18n-amlogic-zh-cn \
+        luci-app-amlogic \
         \
         ${config_list} \
         "

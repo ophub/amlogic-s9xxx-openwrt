d8aab8ecfa07d3fad6e08e3f605f9993596aaaf3
ophub <ophub2020@gmail.com>
2023-03-29 02:45:52 +0000

    Add docker image creation script

---
+++ config-openwrt/docker/make_docker_image.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+#================================================================================================
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of the make OpenWrt
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+#
+# Description: Creating a Docker Image
+# Copyright (C) 2021~ https://github.com/unifreq/openwrt_packit
+# Copyright (C) 2021~ https://github.com/ophub/amlogic-s9xxx-openwrt
+#
+# Command: ./config-openwrt/docker/make_docker_image.sh
+#
+#======================================== Functions list ========================================
+#
+# error_msg         : Output error message
+# find_openwrt      : Find OpenWrt file (openwrt/*rootfs.tar.gz)
+# adjust_settings   : Adjust related file settings
+# make_dockerimg    : make docker image
+#
+#================================ Set make environment variables ================================
+#
+# Set default parameters
+current_path="${PWD}"
+tmp_path="${current_path}/tmp"
+out_path="${current_path}/out"
+openwrt_path="${current_path}/openwrt"
+openwrt_rootfs_file="*rootfs.tar.gz"
+docker_rootfs_file="docker-armvirt-64-default-rootfs.tar.gz"
+make_path="${current_path}/make-openwrt"
+common_files="${make_path}/openwrt-files/common-files"
+
+# Set default parameters
+STEPS="[\033[95m STEPS \033[0m]"
+INFO="[\033[94m INFO \033[0m]"
+SUCCESS="[\033[92m SUCCESS \033[0m]"
+WARNING="[\033[93m WARNING \033[0m]"
+ERROR="[\033[91m ERROR \033[0m]"
+#
+#================================================================================================
+
+error_msg() {
+    echo -e "${ERROR} ${1}"
+    exit 1
+}
+
+find_openwrt() {
+    cd ${current_path}
+    echo -e "${STEPS} Start searching for OpenWrt file..."
+
+    # Find whether the OpenWrt file exists
+    openwrt_file_name="$(ls ${openwrt_path}/${openwrt_rootfs_file} 2>/dev/null | head -n 1 | awk -F "/" '{print $NF}')"
+    if [[ -n "${openwrt_file_name}" ]]; then
+        echo -e "${INFO} OpenWrt file: [ ${openwrt_file_name} ]"
+    else
+        error_msg "There is no [ ${openwrt_rootfs_file} ] file in the [ ${openwrt_path} ] directory."
+    fi
+}
+
+adjust_settings() {
+    cd ${current_path}
+    echo -e "${STEPS} Start adjusting OpenWrt file settings..."
+
+    echo -e "${INFO} Unpack Openwrt."
+    rm -rf ${tmp_path} && mkdir -p ${tmp_path}
+    tar -xzf ${openwrt_path}/${openwrt_file_name} -C ${tmp_path}
+
+    # Remove unused files
+    echo -e "${INFO} Remove useless files."
+    rm -rf ${tmp_path}/lib/firmware/*
+    rm -rf ${tmp_path}/lib/modules/*
+    rm -f ${tmp_path}/usr/lib/lua/luci/controller/amlogic.lua
+    find ${tmp_path} -name '*.rej' -exec rm {} \;
+    find ${tmp_path} -name '*.orig' -exec rm {} \;
+
+    # Turn off hw_flow by default
+    [[ -f "${tmp_path}/etc/config/turboacc" ]] && {
+        echo -e "${INFO} Adjust turboacc settings."
+        sed -i "s|option hw_flow.*|option hw_flow '0'|g" ${tmp_path}/etc/config/turboacc
+        sed -i "s|option sw_flow.*|option sw_flow '0'|g" ${tmp_path}/etc/config/turboacc
+    }
+
+    # Modify the cpu mode to schedutil
+    [[ -f "${tmp_path}/etc/config/cpufreq" ]] && {
+        echo -e "${INFO} Adjust cpufreq settings"
+        sed -i "s/ondemand/schedutil/g" ${tmp_path}/etc/config/cpufreq
+    }
+
+    # Relink the kmod program
+    [[ -f "${common_files}/sbin/kmod" ]] && (
+        echo -e "${INFO} Adjust kmod settings."
+        cp -f ${common_files}/sbin/kmod ${tmp_path}/sbin/kmod
+        chmod +x ${tmp_path}/sbin/kmod
+        kmod_list="depmod insmod lsmod modinfo modprobe rmmod"
+        for ki in ${kmod_list}; do
+            rm -f ${tmp_path}/sbin/${ki}
+            ln -sf kmod ${tmp_path}/sbin/${ki}
+        done
+    )
+
+    # Add version information to the banner
+    [[ -f "${common_files}/etc/banner" ]] && {
+        cp -f ${common_files}/etc/banner ${tmp_path}/etc/banner
+        echo -e "${INFO} Adjust banner settings."
+        echo " Board: docker | Production Date: $(date +%Y-%m-%d)" >>${tmp_path}/etc/banner
+        echo "───────────────────────────────────────────────────────────────────────" >>${tmp_path}/etc/banner
+    }
+}
+
+make_dockerimg() {
+    cd ${tmp_path}
+    echo -e "${STEPS} Start making docker image..."
+
+    # Make docker image
+    tar -czf ${docker_rootfs_file} *
+    [[ "${?}" -eq "0" ]] || error_msg "docker image creation failed."
+
+    # Move the docker image to the output directory
+    rm -rf ${out_path} && mkdir -p ${out_path}
+    mv -f ${docker_rootfs_file} ${out_path}
+    [[ "${?}" -eq "0" ]] || error_msg "docker image move failed."
+
+    echo -e "${INFO} The docker image was created successfully."
+    sync && sleep 3
+
+    # Remove temporary directory
+    cd ${current_path}
+    rm -rf ${tmp_path}
+}
+
+# Show welcome message
+echo -e "${STEPS} Welcome to the Docker Image Maker Tool."
+echo -e "${INFO} Make path: [ ${PWD} ]"
+#
+find_openwrt
+adjust_settings
+make_dockerimg
+#
+# All process completed
+wait

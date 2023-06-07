e2f7a4ceda5d3cb94c4c78e3f7a491a8b8777346
ophub <68696949+ophub@users.noreply.github.com>
2023-06-07 09:03:50 +0000

    Adjusting branch description

--- config-openwrt/immortalwrt-master/diy-part2.sh
+++ config-openwrt/immortalwrt-master/diy-part2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Description: Automatically Build OpenWrt
 # Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-21.02
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------

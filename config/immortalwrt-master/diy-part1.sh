2a06115c608e1a81958264ebea45ec18bb4997a5
T.rex998 <120120275+pppoe1984@users.noreply.github.com>
2023-09-22 22:48:36 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,6 +8,8 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}

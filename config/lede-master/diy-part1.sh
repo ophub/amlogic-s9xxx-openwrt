d813848d5b439813fa4b50197827c76f80d8f037
x2358473 <649056978@qq.com>
2024-06-09 09:53:37 +0800

    Update diy-part1.sh
    添加hellow word

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,6 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}

6c490c9caba9d539d5f1838868e63f6c3092b0c4
casey <89326547+caseybase@users.noreply.github.com>
2024-11-07 09:52:58 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -12,3 +12,8 @@
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default

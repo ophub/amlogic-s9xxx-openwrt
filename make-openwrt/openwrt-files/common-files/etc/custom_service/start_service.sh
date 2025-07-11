b9fa6be7b80deeddd3f1f36af59a74cf913985f1
ophub <68696949+ophub@users.noreply.github.com>
2025-07-11 12:44:45 +0800

    Add OES(A311D) SATA status check script

--- make-openwrt/openwrt-files/common-files/etc/custom_service/start_service.sh
+++ make-openwrt/openwrt-files/common-files/etc/custom_service/start_service.sh
@@ -77,6 +77,12 @@ openvfd_boxid="15"
     echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The fan control service enabled successfully." >>${custom_log}
 }
 
+# For oes(A311d) led color lights
+[[ -x "/usr/bin/oes_sata_leds.sh" ]] && {
+    /usr/bin/oes_sata_leds.sh >/var/log/oes-sata-leds.log 2>&1 &
+    echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The SATA status check on WXY-OES(A311D) enabled successfully." >>${custom_log}
+}
+
 # Automatic expansion of the third and fourth partitions
 todo_rootfs_resize="/root/.todo_rootfs_resize"
 [[ -f "${todo_rootfs_resize}" && "$(cat ${todo_rootfs_resize} 2>/dev/null | xargs)" == "yes" ]] && {

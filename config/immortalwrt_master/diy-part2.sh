5ceb516b9698631c287f4375f6f80ebc463ae1b5
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-01-02 14:32:33 +0800

    Fix shebang and update comments in diy-part2.sh

--- config/immortalwrt_master/diy-part2.sh
+++ config/immortalwrt_master/diy-part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt
@@ -17,7 +17,7 @@ ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01
     sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
 }
 
-# Add the default password for the 'root' user（Change the empty password to 'password'）
+# Add the default password for the 'root' userļ¼Change the empty password to 'password'ļ¼‰
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
@@ -25,21 +25,6 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCEREPO='github.com/immortalwrt/immortalwrt'" >>package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCEBRANCH='master'" >>package/base-files/files/etc/openwrt_release
-
-# Set ccache
-# Remove existing ccache settings
-sed -i '/CONFIG_DEVEL/d' .config
-sed -i '/CONFIG_CCACHE/d' .config
-# Apply new ccache configuration
-if [[ "${2}" == "true" ]]; then
-    echo "CONFIG_DEVEL=y" >>.config
-    echo "CONFIG_CCACHE=y" >>.config
-    echo 'CONFIG_CCACHE_DIR="$(TOPDIR)/.ccache"' >>.config
-else
-    echo '# CONFIG_DEVEL is not set' >>.config
-    echo "# CONFIG_CCACHE is not set" >>.config
-    echo 'CONFIG_CCACHE_DIR=""' >>.config
-fi
 #
 # ------------------------------- Main source ends -------------------------------
 

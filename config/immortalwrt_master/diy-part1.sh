1eea098b4483f0d022e5ded38de3edf4a6a733c9
fengke66 <gtfengkai@gmail.com>
2026-05-03 09:28:54 +0800

    更新 diy-part1.sh

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -3,7 +3,7 @@
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt
 # Function: DIY script (Before updating feeds — modify the default IP, hostname, theme, add/remove packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-24.10
 #========================================================================================================================
 
 # Add a custom feed source
